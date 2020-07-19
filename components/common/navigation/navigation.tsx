50bdd70e549521617745197ee8aae341d6aef8ee
Rajendra <rajendra@Rajendras-MacBook-Pro.local>
2020-07-19 13:02:16 +0530

    active routes

--- components/common/navigation/navigation.tsx
+++ components/common/navigation/navigation.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Nav} from 'react-bootstrap';
 import Link from 'next/link';
+import {useRouter} from 'next/router';
 
  interface routes {
     name:string;
@@ -10,10 +11,12 @@ import Link from 'next/link';
 
 function Navigation(props:any) {
     const {navItems} = props;
+    const router = useRouter();
     return (
         <Nav className="mr-auto">
             {navItems.map((nav:routes)=>{
-                return <Link href={nav.route}><a className="nav-link" href="#">{nav.name} <span className="sr-only">(current)</span></a></Link>
+                const isSelected = router.pathname === nav.route || router.pathname.indexOf(nav.name.toLowerCase())>-1;
+                return <Link href={nav.route}><a className={isSelected ? "nav-link route-selected"  : "nav-link"} href="#">{nav.name} <span className="sr-only">(current)</span></a></Link>
             })}
         </Nav>
     )
