f1196eca8ec87762f6c45ee42b17339a6b20e6f9
Rajendra <rajendra@Rajendras-MacBook-Pro.local>
2020-07-20 17:27:17 +0530

    homepage banner implemented

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 
 import Layout from '../../components/layout'
+import { Col, Row } from 'react-bootstrap'
 
 function Home() {
     return (
         <Layout>
-            {/* <h5>This is home page</h5> */}
+            <section>
+                <Row>
+                <Col xs={12} md={5}>
+                    <p>sidebar</p>
+                </Col>
+                <Col xs={12}  md={7}>
+                    <img src="/images/home-banner.jpg" title="home-banner" alt="home-banner" />
+                </Col>
+                </Row>
+            </section>
         </Layout>
     )
 }
