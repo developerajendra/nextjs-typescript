8a1cbe7f58ac13ceb8b134614db15e23f401a93e
Rajendra <rajendra@Rajendras-MacBook-Pro.local>
2020-07-23 12:44:45 +0530

    carousel UI

--- components/testimonialCarousel/testimonialCarousel.tsx
+++ components/testimonialCarousel/testimonialCarousel.tsx
@@ -9,7 +9,7 @@ function TestimonialCarousel() {
                 <Carousel.Item>
                     <Row>
                         <Col lg={5}>
-                            <Col lg={8}>
+                            <Col className="select-wrapper" lg={7}>
                                 <div className="list-header">
                                     <span>Happy to share</span>
                                     <h2>Patient Stories</h2>
@@ -17,11 +17,20 @@ function TestimonialCarousel() {
                                 <DropDown label="SELECT COUNTRY"/>
                                 <DropDown label="SELECT STATE"/> 
                             </Col>
-                        <div className="testimonials">
-                            <h3>Abhimanyu Das Patel</h3>
-                            <address>  Saket, New Delhi</address>
-                            <article>“ Excellent service during my stay, he also went out of the way to assist me and my wife. I can not suggest any thing better in the service.Excellent work by team, thank you so much “</article>
-                        </div>
+                            <Col className="select-wrapper" lg={10}>
+                                <div className="testimonials">
+                                    <h3>Abhimanyu Das Patel</h3>
+                                    <address>  Saket, New Delhi</address>
+                                    <ul>
+                                        <li> <i className="icon-rated-star"></i> </li>
+                                        <li> <i className="icon-rated-star"></i></li>
+                                        <li> <i className="icon-rated-star"></i></li>
+                                        <li> <i className="icon-rated-star"></i></li>
+                                        <li className="not-rated"> <i className="icon-rated-star"></i></li>
+                                    </ul>
+                                    <article>“ Excellent service during my stay, he also went out of the way to assist me and my wife. I can not suggest any thing better in the service.Excellent work by team, thank you so much “</article>
+                                </div>
+                            </Col>
                         
                         </Col>
                         <Col lg={7}>
@@ -36,67 +45,7 @@ function TestimonialCarousel() {
                     </Row>
                 </Carousel.Item>
 
-                 
-                <Carousel.Item>
-                    <Row>
-                        <Col lg={5}>
-                            <Col lg={8}>
-                                <div className="list-header">
-                                    <span>Happy to share</span>
-                                    <h2>Patient Stories</h2>
-                                </div>
-                                <DropDown label="SELECT COUNTRY"/>
-                                <DropDown label="SELECT STATE"/> 
-                            </Col>
-                        <div className="testimonials">
-                            <h3>Abhimanyu Das Patel</h3>
-                            <address>  Saket, New Delhi</address>
-                            <article>“ Excellent service during my stay, he also went out of the way to assist me and my wife. I can not suggest any thing better in the service.Excellent work by team, thank you so much “</article>
-                        </div>
-                        
-                        </Col>
-                        <Col lg={7}>
-                            <i className="quotes-top icon-carousel-quotes"></i>
-                             <img
-                            className="carousel-image"
-                            src="/images/carousel-image.png"
-                            alt="First slide"
-                            />
-                            <i className="quotes-bottom icon-carousel-quotes"></i>
-                        </Col>
-                    </Row>
-                </Carousel.Item>
-
-
-                <Carousel.Item>
-                    <Row>
-                        <Col lg={5}>
-                            <Col lg={8}>
-                                <div className="list-header">
-                                    <span>Happy to share</span>
-                                    <h2>Patient Stories</h2>
-                                </div>
-                                <DropDown label="SELECT COUNTRY"/>
-                                <DropDown label="SELECT STATE"/> 
-                            </Col>
-                        <div className="testimonials">
-                            <h3>Abhimanyu Das Patel</h3>
-                            <address>  Saket, New Delhi</address>
-                            <article>“ Excellent service during my stay, he also went out of the way to assist me and my wife. I can not suggest any thing better in the service.Excellent work by team, thank you so much “</article>
-                        </div>
-                        
-                        </Col>
-                        <Col lg={7}>
-                            <i className="quotes-top icon-carousel-quotes"></i>
-                             <img
-                            className="carousel-image"
-                            src="/images/carousel-image.png"
-                            alt="First slide"
-                            />
-                            <i className="quotes-bottom icon-carousel-quotes"></i>
-                        </Col>
-                    </Row>
-                </Carousel.Item>
+                  
                 </Carousel>
         </div>
     )
